25c72b039e9d878942fe35af237f0cb775e30695
LzGustavo <xavier.luizgustavo@gmail.com>
2020-11-12 08:59:57 -0300

    implemented delete, complete, and restore tasks

--- api.go
+++ api.go
@@ -77,8 +77,13 @@ func tasksHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 	case "DELETE":
-		// TODO: parse id
-		err := deleteTask(r.Context(), usr, 0)
+		id, err := parseIDFromArgs(args)
+		if err != nil {
+			http.Error(w, "error parsing args on delete operation, err:"+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = deleteTask(r.Context(), usr, id)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -96,7 +101,24 @@ func tasksCompleteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO
+	args := r.URL.Query()
+	if _, ok := args["usr"]; !ok {
+		http.Error(w, "no user informed", http.StatusBadRequest)
+		return
+	}
+	usr := args["usr"][0]
+
+	id, err := parseIDFromArgs(args)
+	if err != nil {
+		http.Error(w, "error parsing args on complete task operation, err:"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = completeTask(r.Context(), usr, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func tasksRestoreHandler(w http.ResponseWriter, r *http.Request) {
@@ -105,17 +127,42 @@ func tasksRestoreHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO
+	args := r.URL.Query()
+	if _, ok := args["usr"]; !ok {
+		http.Error(w, "no user informed", http.StatusBadRequest)
+		return
+	}
+	usr := args["usr"][0]
+
+	id, err := parseIDFromArgs(args)
+	if err != nil {
+		http.Error(w, "error parsing args on restore task operation, err:"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = restoreTask(r.Context(), usr, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
-func parseUpdateArgs(args url.Values) (int, string, string, error) {
+func parseIDFromArgs(args url.Values) (int, error) {
 	if _, ok := args["id"]; !ok {
-		return 0, "", "", errors.New("no id informed for update operation")
+		return 0, errors.New("no id informed for update operation")
 	}
 
 	id, err := strconv.Atoi(args["id"][0])
 	if err != nil {
-		return 0, "", "", errors.New("could not parse id value informed")
+		return 0, errors.New("could not parse id value informed")
+	}
+	return id, nil
+}
+
+func parseUpdateArgs(args url.Values) (int, string, string, error) {
+	id, err := parseIDFromArgs(args)
+	if err != nil {
+		return 0, "", "", err
 	}
 
 	if _, ok := args["desc"]; !ok {
